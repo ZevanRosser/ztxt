88d7e474777aa13de93419659894397db2660c63
Zevan Rosser <bendvent@gmail.com>
2013-02-08 23:26:32 -0500

    removing more old files

--- loadfile.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (isset($_POST["name"])){
-  $file = file_get_contents($_POST["name"]);// or die("There was an error loading " . $_POST["name"] . " you should check your permissions to make sure
-    //that php can write to your projects directory. On OSX try running the terminal command chmod -R 777 projects.");
-  
-  echo $file;
-}
